7cbf405050436a83c8e6a923330277bf0b48cf4b
toybreaker <toybreaker@users.noreply.github.com>
2020-08-16 15:34:02 +0800

    refactored completely

--- gulpfile.js
+++ gulpfile.js
@@ -1,91 +1,148 @@
 'use strict';
-// images task to optimize images.
+// GULP tasks to SIZE AND RENAME images.
+var changeCase   = require('change-case-all');
 var gulp         = require('gulp');
+var rename       = require('gulp-rename');
 var responsive   = require('gulp-responsive');
-var imagemin     = require('gulp-imagemin');
+var del          = require('del');
 
 
-// Reponsive sizing DEFAULT
-gulp.task('default', function () {
-  return gulp.src('./BIM/*.jpg')
+// WHAT: adds folder name + index, lowercasing the original file name.
+// starts from 10, to avoid single digit no. to the first 9 jpgs
+var index = 10;
+gulp.task('curatename', function (done) {
+  // put image folders w/ proper name into _images_to_rename
+  return gulp.src('./_input/_images_to_rename/**/*.jpg')
+  .pipe(rename(function(fix) {
+     fix.basename = changeCase.lowerCase(fix.basename);
+   }))
+  .pipe(rename(function (path) {
+    // prefix w/ folder name + suffix w/ index (starting at 10!)
+    path.basename =  (path.dirname + '-' + path.basename + '-' + index++);
+  }))
+  // output to _images_to_process folder for next step
+  .pipe(gulp.dest('./_output/'));
+});
+
+// WHAT: delete the original file name + name them using folder name + index .
+// to avoid single digit no. to the first 9 jpgs, start from 10, intead of 0
+var index = 0;
+gulp.task('rename_images', function (done) {
+  // put image folders w/ proper name into _images_to_rename
+  return gulp.src('./_input/_images_to_rename/**/*.jpg')
+  .pipe(rename(function (path) {
+    // prefix w/ folder name + suffix w/ index (starting at 10!)
+    path.basename =  (path.dirname + '-' + index++);
+  }))
+  // output to _images_to_process folder for next step
+  .pipe(gulp.dest('./_output/'));
+});
+
+// Reponsive sizing w/ gulp4
+// NOTE: Does transfer folder and lowercase the jpgs names
+// OK!
+gulp.task('size_images', function (done) {
+  return gulp.src('./_input/_images_to_size/**/*.jpg')
+    .pipe(rename(function(fix) {
+       fix.basename = changeCase.lowerCase(fix.basename);
+     }))
     .pipe(responsive({
-      '*.jpg': [{
-        //nexus5
+      '**/*.jpg': [{
         width: 640,
-        quality: 61,
+        quality: 55,
         progressive: true,
         sharper: true,
         rename: {
           suffix: '-640'
         }
       }, {
-        //ipad
+        width: 880,
+        quality: 44,
+        progressive: true,
+        rename: {
+          suffix: '-880'
+        }
+      }, {
         width: 1024,
-        quality: 66,
+        quality: 44,
         progressive: true,
         rename: {
           suffix: '-1024'
         }
       }, {
-        //average laptop screen
-        width: 1680,
-        quality: 66,
-        progressive: true,
-        rename: {
-          suffix: '-1680'
-        }
-      }, {
         //fullHD
         width: 1920,
-        quality: 71,
+        quality: 33,
         progressive: true,
         rename: {
           suffix: '-1920'
         }
-      }]
+      }, {
+        //named same as original for use with jekyll_seo Open Graph / Twitter Cards
+        width: 1024,
+        quality: 44,
+        progressive: true
+      }],
+    }, {
+      // global configuration for all images
+      errorOnEnlargement: false,
+      withMetadata: false,
+      withoutEnlargement: false,
+      //try this, sometimes doesn't work
+      //withoutChromaSubsampling: true
     }))
-    .pipe(gulp.dest('./BAM/'));
+    // this is needed otherwise it outputs .jpeg, gosh...
+    .pipe(rename(function(fix) {
+      fix.extname = '.jpg';
+    }))
+    // puy jpgs ready in place for SSG to use
+    .pipe(gulp.dest('./_output/'));
+  done();
+});
+
+// Rename all to lowercase w/ gulp4
+// OK! (is this used in other tasks? ops, i forgot!)
+gulp.task(function lowercase(done) {
+  return gulp.src( './_input/_images_to_lowercase/**/*.*' )
+    .pipe(rename(function(fix) {
+       fix.basename = changeCase.lowerCase(fix.basename);
+     }))
+
+    .pipe(gulp.dest( './_input/_images_to_size/' ));
+  done();
+});
+
+// DELETE TASKS
+
+// empty "images_to_lowercase"
+gulp.task('delete_images_to_lowercase_dir_content', function(done) {
+    del(['./_input/_images_to_lowercase/**']);
+  done();
+});
+
+// empty "_images_to_size"
+gulp.task('delete_images_to_size_dir_content', function(done) {
+    del(['./_input/_images_to_size/**']);
+  done();
+});
+
+// empty "_images_to_rename"
+gulp.task('delete_images_to_rename_dir_content', function(done) {
+    del(['./_input/_images_to_rename/**']);
+  done();
 });
 
 
+// PROCESS+DELETE COMMANDS
 
-// make Fumes thumbs smaller
-gulp.task('fumes', function () {
-  return gulp.src('./BIM/*.jpg')
-    .pipe(responsive({
-      '*.jpg': [{
-        //tinythumbs
-        width: 180,
-        quality: 61,
-        progressive: true,
-        sharper: true
-      }]
-    }))
-    .pipe(gulp.dest('./BAM/'));
-});
+// Rename all to lowercase + del
+gulp.task('lower', gulp.series(['lowercase', 'delete_images_to_lowercase_dir_content']));
 
+// Produce all the different sizes images + del
+gulp.task('sizes', gulp.series(['size_images', 'delete_images_to_size_dir_content']));
 
+// Rename images with dir name and progressive index + del
+gulp.task('rename', gulp.series(['rename_images', 'delete_images_to_rename_dir_content']));
 
-// variations on DEFAULT gulp task for LANDI
-gulp.task('bim', function () {
-  return gulp.src('./BIM/*.jpg')
-    .pipe(responsive({
-      '*.jpg': [{
-        //average laptop screen
-        width: 1680,
-        quality: 33,
-        progressive: true,
-        rename: {
-          prefix: 'raku-1680-'
-        }
-      }]
-    }))
-    .pipe(gulp.dest('./BUM/'));
-});
-
-// SQUEEZE 'em
-gulp.task('bum', () =>
-    gulp.src('./BUM/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./BAM'))
-);
+// Rename images adding dir name and progressive index + del
+gulp.task('curate', gulp.series(['curatename', 'delete_images_to_rename_dir_content']));
--- readme.md
+++ readme.md
@@ -1,19 +1,103 @@
-**Grind and squeeze 'em all** with BIM BUM BAM
+**Image processor.** Outputs responsive image sets to use with _srcset_
 
-with:
+Currently using:
 
 - [gulp-responsive](https://github.com/mahnunchik/gulp-responsive)
 
-- [imagemin](https://github.com/sindresorhus/gulp-imagemin)
+- [gulp-rename](https://github.com/hparra/gulp-rename)
+
+- [del](https://github.com/sindresorhus/del)
+
+- [change-case](https://github.com/blakeembrey/change-case)
 
 
 _Note:_ gulp-responsive depends on [sharp](https://github.com/lovell/sharp). Sharp is one of the fastest Node.js modules for resizing JPEG, PNG, WebP and TIFF images.
 
 **How to use it?**
----
-- download/clone
-- npm install
-- copy jpegs > BIM
-- gulp
-- look in BUM for grinded ones
-- and in BAM for squeezed ones!
+
+### Install node dependencies into project's local dir.
+
+```sh
+npm install
+```
+
+## Tasks without deleting originals.
+
+### **(size_images)** Produce images sized 640, 880, 1024, 1920.**
+
+**WHAT:**
+Produce sized images and lowercase the file names. Output also one image with no suffix, named same as original, to use with jekyll_seo Open Graph / Twitter Cards.
+
+**HOW:**
+Put images into ```_input/_images_to_size/``` then run:
+
+```sh
+gulp size_images
+```
+
+### **(curatename)** Curate images names.**
+
+**WHAT:**
+Adds folder name + index, lowercasing the original file name.
+Starts from 10, to avoid single digit no. in the first 9 jpgs.
+Edit gulp files to start from 0 if you prefer.
+
+**HOW:**
+Put images into ```_input/_images_to_rename/``` then run:
+
+```sh
+gulp curatename
+```
+
+### **(rename_images)** Rename images.
+
+**WHAT:**
+Changes the original file name and name images using folder name + index .
+To avoid single digit no. for the first 9 jpgs, set ```var index = 10;``` in the gulpfile.js.
+
+**HOW:**
+Put images into ```_input/_images_to_rename/``` then run:
+
+```sh
+gulp rename_images
+```
+
+
+
+
+## Tasks which delete source images
+
+
+### **(sizes)** Produce images sized 640, 880, 1024, 1920 + del.
+
+**WHAT:**
+Output also one image with no suffix, named same as original, to use with jekyll_seo Open Graph / Twitter Cards.
+
+**HOW:**
+Put images into ```_input/_images_to_size/``` then run:
+
+```sh
+gulp sizes
+```
+
+### **(rename)** Rename images with dir name and progressive index + del.**
+
+```sh
+gulp rename
+```
+
+### **(curate)** Rename images adding dir name and progressive index + del**
+
+```sh
+gulp curate
+```
+
+
+### **(lower)** lowercase images file name + del**
+
+**WHAT:**
+Output into ```_input/_images_to_size/```.
+
+```sh
+gulp lower
+```
